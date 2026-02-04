61a22b18b9b795ca5d8b5c4f9e972cc0f75dcbbb
GD-Finn <thiagocarvalholima74@gmail.com>
2026-02-04 20:46:23 -0300

    Create script.js
    primeiro script(iniciante)
    Curso de Logica de Progamação

---
+++ ex001/script.js
@@ -0,0 +1,22 @@
+var x = 5; 
+var y = 10;
+var z = 5;
+
+var resultado = ( x + y) / z;
+
+console.log(`resultado da equação e:  ${resultado}`);
+
+var a = 5 + 5;
+var b = 10 - 2;
+var c = 4 * 3;
+var d = 20 / 5;
+var e = 2**2 /* expoente */
+var f = 10 % 2; /* resto de divisão */
+
+console.log(`Resultados: ${a} ${b} ${c} ${d} ${e} ${f}`)
+
+var media = 7;
+var status = media >= 9 ? "Aprovado " : "Reprovado";
+
+console.log(` media = 6
+Status do aluno: ${status} `);
