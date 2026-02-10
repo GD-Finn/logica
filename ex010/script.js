5b2d974f54ea4ece2781b0201e7998d53e9392ed
GD-Finn <thiagocarvalholima74@gmail.com>
2026-02-09 21:36:43 -0300

    dia 5 codigo do professor (nao entendi muito)
    ALGUEM ME AJUDAAAA

--- ex010/script.js
+++ ex010/script.js
@@ -3,17 +3,21 @@ var nome = document.getElementById("nomeAluno")
 var lista = document.getElementById("resultado")
 
 function cadastrar() {
+    if (nome.value === ""){
+        alert("Por favor, insira o nome do aluno.");
+    } else {
     if (nomes.indexOf(nome.value) === -1) {
          nomes.push(nome.value);
          alert("Aluno cadastrado com sucesso!");
     } else {
-        alert("Aluno ja cadastrado!")
+        alert("Aluno ja cadastrado!");
+       }
     }
 }
 
 function listar() {
-        for0 (var i = 0; i < nomes.length; i++) {
-            
+        for (var i = 0; i < nomes.length; i++) {
+          lista.innerHTML += `Aluno - ${nomes[i]} <br>`;
         }
     
 } 
